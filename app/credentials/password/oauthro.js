34bea05ca718414ee57eba4d1b4f721125996992
Jared Hanson <jaredhanson@gmail.com>
2019-05-04 12:23:35 -0700

    Update RO.

--- app/credentials/password/oauthro.js
+++ app/credentials/password/oauthro.js
@@ -1,4 +1,4 @@
-exports = module.exports = function() {
+exports = module.exports = function(creds) {
   var Client = require('../../../lib/authentication/legacy/roclient');
   
   
@@ -6,12 +6,20 @@ exports = module.exports = function() {
   
   // TODO: add inferType from URL method
   
-  api.createConnection = function(options) {
-    return new Client(options.url);
+  api.createConnection = function(options, connectListener) {
+    var client = new Client(options.url);
+    client._creds = creds;
+    
+    client.connect(connectListener);
+    return client;
   }
   
   return api;
 };
 
+exports['@singleton'] = true;
 exports['@implements'] = 'http://schemas.authnomicon.org/sd/IService';
 exports['@type'] = 'auth0-oauth-ro';
+exports['@require'] = [
+  'http://i.bixbyjs.org/security/CredentialsStore'
+];
