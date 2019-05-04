9276b573e204182db4ee6eb95e300cb1339133e6
Jared Hanson <jaredhanson@gmail.com>
2019-05-04 16:38:11 -0700

    Add tests.

--- test/credentials/password/oauth2token.test.js
+++ test/credentials/password/oauth2token.test.js
@@ -58,4 +58,67 @@ describe('credentials/password/oauth2token', function() {
     
   }); // API
   
+  describe('PasswordClient', function() {
+    var _client = sinon.createStubInstance(AuthenticationClient);
+    var ClientStub = sinon.stub().returns(_client);
+    var Client = $require('../../../lib/authentication/oauth/passwordclient',
+      { 'auth0': { AuthenticationClient: ClientStub } }
+    );
+    
+    describe('#connect', function() {
+      
+      it('should get credential and construct client', function(done) {
+        var client = new Client('https://hansonhq.auth0.com');
+        client._creds = new StubCredentialStore();
+        sinon.stub(client._creds, 'get').yieldsAsync(null, { username: 'wvaTP5EkEjKxGyLAIzUnsnG6uhyRUTkX', password: 'keyboard cat' });
+        
+        client.connect(function() {
+          expect(client._creds.get).to.have.been.calledOnceWith('https://hansonhq.auth0.com');
+          
+          expect(ClientStub).to.have.been.calledOnceWithExactly({
+            domain: 'hansonhq.auth0.com',
+            clientId: 'wvaTP5EkEjKxGyLAIzUnsnG6uhyRUTkX',
+            clientSecret: 'keyboard cat'
+          }).and.calledWithNew;
+          
+          done();
+        });
+      }); // should get credential and construct client
+      
+    }); // #connect
+    
+    describe('#verify', function() {
+      var client = new Client('https://hansonhq.auth0.com');
+      client._creds = new StubCredentialStore();
+      
+      beforeEach(function(done) {
+        sinon.stub(client._creds, 'get').yieldsAsync(null, { username: 'wvaTP5EkEjKxGyLAIzUnsnG6uhyRUTkX', password: 'keyboard cat' });
+        client.connect(done);
+      });
+      
+      it('should verify correct credentials', function(done) {
+        _client.oauth = {};
+        _client.oauth.passwordGrant = sinon.stub().yieldsAsync(null, JSON.parse(fs.readFileSync('test/data/oauth/token/output.json', 'utf8')));
+        
+        
+        client.verify('steve@example.com', 'p455w0rd', function(err, user) {
+          expect(_client.oauth.passwordGrant).to.have.been.calledWith({
+            username: 'steve@example.com',
+            password: 'p455w0rd',
+            realm: 'Username-Password-Authentication'
+          });
+          
+          expect(err).to.be.null;
+          expect(user).to.deep.equal({
+            id: 'auth0|5b6ce4a9e54355613fd4627c',
+            displayName: 'steve@example.com'
+          });
+          done();
+        });
+      }); // should verify correct credentials
+      
+    }); // #verify
+    
+  }); // PasswordClient
+  
 });
